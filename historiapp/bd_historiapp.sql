0a8a2c2dd8b890b48aa65578a173e61b67a749a7
Ronaldo Stive Guette Ospino <rsguetteospino@gmail.com>
2021-10-20 14:24:36 -0500

    modificar

--- historiapp/bd_historiapp.sql
+++ historiapp/bd_historiapp.sql
@@ -6,9 +6,6 @@ CREATE TABLE usuario1 (
   id_usuario1 INT NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(45) NOT NULL,
   apellido VARCHAR(45) NOT NULL,
-  tipo_documento VARCHAR(20) NOT NULL,
-  numero_documento VARCHAR(20) NOT NULL,
-  correo VARCHAR(45) NOT NULL,
   usuario VARCHAR(20) NOT NULL,
   contraseña BINARY(20) NOT NULL,
   CONSTRAINT pk_usuario1 PRIMARY KEY (id_usuario1)
